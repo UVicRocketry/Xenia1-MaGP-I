16c58850a93108464caad6bbda283c9e5ff37b9f
cristyro <95331909+cristyro@users.noreply.github.com>
2022-07-22 22:28:01 -0700

    Update MAGPIcamera.py
    This new version of the code handles both cameras and gets triggered by the GPIO pin received from the Arduino

--- MAGPIcamera.py
+++ MAGPIcamera.py
@@ -1,4 +1,5 @@
-#This is the draft for the camera stream 
+     
+ #This is the draft for the camera stream 
 
 import io
 import random
@@ -12,41 +13,23 @@ from picamera import PiCamera
 from smbus import SMBus #to work with 12C communication
 
 
-camera = PiCamera()
-camera.resolution = (1024, 760)
-camera.start_preview()
-camera.start_recording('video.h264')
-sleep(5)
-camera.stop_recording()
-camera.stop_preview()
+pi_camera1 = VideoStream(usePiCamera=True).start()
+pi_camera2 = VideoStream(usePiCamera=True).start()
 
-def start_serial():
-    ser=serial.Serial('/dev/ttyACM!', 9600,timeout=1)
-    #ttyACM1 is the name of arduino in port,might need to change
-    #timeout to protect program
-    while True:
-        ser_data=ser.readline() 
-        ser_data.flush()
-        return ser_data
-    #store ser_data for function use 
     
-
-def motion_detected():
-    data= start_serial()
-    if(data!="Acceleration data"):
-        accel_x, accel_y, accel_z=data 
-        if (int(accel_x)!=0 and int(accel_y)!=0 and int(accel_z)!=0):
-            camera = picamera.PiCamera()
-            stream = picamera.PiCameraCircularIO(camera, seconds=20)
-            camera.start_recording(stream, format='h264')
-            try:
+if(GPIO_pin==high):
+        camera1= picamera.PiCamera()
+        camera2= picamera.PiCamera()
+        
+        stream1 = camera1.PiCameraCircularIO(camera)
+        stream2 = camera2.PiCameraCircularIO(camera)
+        camera1.start_recording(stream, format='h264')
+        try:
                 while True:
-                    camera.wait_recording(1)
-                    if motion_detected():
-                        # Keep recording for 10 seconds and only then write the
-                        # stream to disk
+                        camera.wait_recording(1)
                         camera.wait_recording(10)
                         stream.copy_to('motion.h264')
-            finally:
+        finally:
                 camera.stop_recording()            
  
+
